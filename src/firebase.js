554a4ce035cb033a351f72d749636e30b17bcabc
praveen97422 <161826204+praveen97422@users.noreply.github.com>
2025-05-18 19:01:49 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAkTcT7IXbuKnQEimCb1rV5mH7CrQ4AvSQ",
-  authDomain: "loginhavya.firebaseapp.com",
-  projectId: "loginhavya",
-  storageBucket: "loginhavya.firebasestorage.app",
-  messagingSenderId: "521485794547",
-  appId: "1:521485794547:web:f55574324b63b20c10cb86",
-  measurementId: "G-6RHW3LMYF6"
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
